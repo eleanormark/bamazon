bf29d2e9f538f7ba64134112d944a921730518e7
Eleanor Mark <emark@Eleanors-MBP.attlocal.net>
2017-04-23 00:00:23 -0700

    added output info

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,6 +4,7 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 var Table = require('cli-table');
 
+var shoppingCart = [];
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -13,11 +14,6 @@ var connection = mysql.createConnection({
 });
 
 //new cli-table
-var inventoryTable = new Table({
-    head: ['ID', 'Product', 'Category', 'Price', 'quantity']
-    , colWidths: [10, 40, 20, 20, 10]
-});
-
 var customerViewTable = new Table({
     head: ['ID', 'Product', 'Category', 'Price']
     , colWidths: [10, 40, 20, 20]
@@ -25,15 +21,15 @@ var customerViewTable = new Table({
 
 connection.connect(function (err) {
     if (err) {
-        console.log("whoops something went wrong");
+        console.log("something went wrong");
         console.log(err);
     } else {
         // console.log('Yay, we connected ' + connection.threadId)
-        createTables(printConsumerViewTable,promptConsumer);
+        createTables(promptConsumer);
     }
 });
 
-function createTables(callback1, callback2){
+function createTables(callback){
     //get all rows from the Products table
     connection.query('SELECT * FROM Products', function(err, res){
         if (err) throw err;
@@ -42,23 +38,15 @@ function createTables(callback1, callback2){
         //add all of the rows to the cli-table
         res.forEach(function (row) {
 
-            inventoryTable.push(
-                [row.item_id, row.product_name, row.department_name, row.price, row.stock_quantity]
-            );
-
             customerViewTable.push(
                 [row.item_id, row.product_name, row.department_name,  '$'+ row.price]
             );
         });
-        callback1();
-        callback2();
+
+        console.log(customerViewTable.toString());
+        callback();
 
     });
-}
-
-function printConsumerViewTable (){
-        //log the table to the console
-        console.log(customerViewTable.toString());
 }
 
 function promptConsumer (){
@@ -69,7 +57,7 @@ function promptConsumer (){
             message: 'Enter item ID from the table: ',
             validate: function (value) {
                 var value = parseInt(value);
-                return value >= 1 && value < inventoryTable.length;
+                return value >= 1 && value < customerViewTable.length;
             }
         },
         {
@@ -83,29 +71,24 @@ function promptConsumer (){
         }
 
     ]).then(function (answers) {
-        checkAndInventory(answers.item_id, answers.quantity);
+        checkAndUpdateInventory(answers.item_id, answers.quantity);
     });
-
 }
 
-function checkAndInventory (id, quantity) {
+function checkAndUpdateInventory (id, quantity) {
     connection.query('SELECT * FROM Products WHERE item_id = ?', [id], function(err, res){
-        if (err) throw err;
-
         var stockQuantity = res[0].stock_quantity;
-
+        if (err) throw err;
+        var currentItem = res;
         if (quantity <= stockQuantity) {
 
             connection.query('UPDATE PRODUCTS SET stock_quantity = stock_quantity - ? WHERE item_id = ?', [ quantity, id], function (err, res) {
-
+                if (err) throw err;
+                console.log("subtotal: " + currentItem[0].product_name + " " + currentItem[0].price + " x " + quantity + " = $" + currentItem[0].price * quantity);
             } );
         } else {
             console.log("Sorry, we have: " + stockQuantity + "in stock.");
         }
-        createTables(printConsumerViewTable,promptConsumer);
     });
 
 }
-
-
-
